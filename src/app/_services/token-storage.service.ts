4f7932b239ad93d530cb4e36d78108b1281f2896
sreesuda2567 <sreesuda.s@rmutsv.ac.th>
2026-06-26 16:52:11 +0700

    26may

--- src/app/_services/token-storage.service.ts
+++ src/app/_services/token-storage.service.ts
@@ -35,7 +35,7 @@ export class TokenStorageService {
     const user = window.sessionStorage.getItem(USER_KEY);
     if (user) {
       return JSON.parse(user);
-     /* const parsedUser = JSON.parse(user);
+      /*const parsedUser = JSON.parse(user);
       parsedUser.citizen = '3909900110443'
       return parsedUser;*/
     }
